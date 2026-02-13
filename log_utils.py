eda0bb71ee4adf21783f086bc27c0085246e6419
Anthony Arminio <tonyarminio.0@gmail.com>
2026-02-12 21:12:34 -0500

    creates log_function_call function which builds a JSON String from input parameters

--- log_utils.py
+++ log_utils.py
@@ -1,10 +1,33 @@
+import json
+
+count_log_file_path = "log/count_log.txt"
+call_log_file_path = "log/call_log.txt"
+user_log_file_path = "log/user_log.txt"
+
+# Creates a JSON String containing the details of a function call and appends the String to a file.
+# function_name: the canonical name of the function
+# function_params: a dictionary containing the parameters of interest used when calling the function
+def log_function_call(function_name, function_params):
+
+    call_number = read_counts()[function_name]
+    increment_count(function_name)
+
+    fc_entry = {
+        "function_name":function_name,
+        "call_number":call_number,
+        "parameters":function_params
+    }
+
+    json_str = json.dumps(fc_entry)
+    print("JSON:", json_str)
+
 # Increments the counter for a specified metric in a given log file. Returns the new count and 
 # returns 0 if the keyname does not match any recognizable keyname in the log file.
-def increment_count(log_file_path, keyname, amount=1):
+def increment_count(keyname, amount=1):
     try:
-        counts = read_counts(log_file_path)
+        counts = read_counts(count_log_file_path)
         counts[keyname] += amount
-        write_counts(log_file_path, counts)
+        write_counts(count_log_file_path, counts)
         return counts[keyname]
     except:
         return 0
@@ -15,13 +38,13 @@ def increment_count(log_file_path, keyname, amount=1):
 # Key_1[\t]Value_1
 # Key_2[\t]Value_2
 # ...
-def read_counts(log_file_path):
+def read_counts():
     import os
 
     counts = {}
 
-    if os.path.exists(log_file_path):
-        with open(log_file_path, "r") as file:
+    if os.path.exists(count_log_file_path):
+        with open(count_log_file_path, "r") as file:
             for line in file:
                 key, value = line.strip().split('\t')
                 counts = {**counts, key: int(value)}
@@ -29,10 +52,10 @@ def read_counts(log_file_path):
     return counts
 
 # Writes a counts dictionary to a log file
-def write_counts(log_file_path, counts):
+def write_counts(counts):
     import os
 
-    with open(log_file_path, "w") as file:
+    with open(count_log_file_path, "w") as file:
         for key, value in counts.items():
             file.write(f"{key}\t{value}\n")
 

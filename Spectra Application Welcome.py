0c538841df1af9873f1ff6542208daa005698f81
Anthony Arminio <tonyarminio.0@gmail.com>
2026-03-15 16:07:43 -0400

    implements rename (from 'tony') and removes old login code

--- Spectra Application Welcome.py
+++ Spectra Application Welcome.py
@@ -15,17 +15,6 @@ os.chdir(current_dir)
 
 function.wide_space_default()
 
-# Request authentication
-if not st.user.is_logged_in:
-    if st.button("Log in with Google"):
-        st.login()
-    st.stop()
-
-st.text("Hi " + st.user.name + "!")
-
-if st.button("Log out"):
-    st.logout()
-
 # hide_st_style = """
 #             <style>
 #             #MainMenu {visibility: hidden;}

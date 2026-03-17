a1bd98d648b22df7c5e1d495d5c2d90fecae7998
AnthonyArminio <tonyarminio.0@gmail.com>
2026-03-17 11:51:34 -0400

    Feature/clerk signup (#14)
    * fixes sign up widget not rendering
    
    * sync
    
    * fixes google signup bug
    
    * adds dynamic height system to Clerk login widgets

--- frontend/src/ClerkComponent.tsx
+++ frontend/src/ClerkComponent.tsx
@@ -4,13 +4,12 @@ import {
   ComponentProps,
 } from "streamlit-component-lib"
 import React, {
-  useCallback,
   useEffect,
-  useMemo,
+  useRef,
   useState,
   ReactElement,
 } from "react"
-import { SignIn } from "@clerk/clerk-react"
+import { SignIn, SignUp } from "@clerk/clerk-react"
 import { useUser, useClerk } from "@clerk/clerk-react"
 
 const COMPONENT_URL = import.meta.env.VITE_COMPONENT_HOST_URL
@@ -31,17 +30,36 @@ function ClerkComponent({ args, theme }: ComponentProps): ReactElement {
 
   // Extract custom arguments passed from Python
   const action = args["action"]
-  const height = args["height"]
+  const heightOffset = args["height_offset"]
+  const heightMinimum = args["min_height"]
+  const visible = args["visible"]
 
   const { isSignedIn, user } = useUser()
   const { signOut } = useClerk()
+  const params = new URLSearchParams(window.location.search);
+  const mode = params.get("mode") == "signup" ? "signup" : "signin";
 
+  const containerRef = useRef(null)
+
+  // check for a resize
   useEffect(() => {
-    // Call this when the component's size might change
-    Streamlit.setFrameHeight(height)
-    // Adding the style and theme as dependencies since they might
-    // affect the visual size of the component.
-  }, [theme])
+    const element = containerRef.current;
+    if (!element) return;
+
+    const observer = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        const height = entry.contentRect.height + heightOffset > heightMinimum ? entry.contentRect.height + heightOffset : heightMinimum;
+        if (visible) {
+          Streamlit.setFrameHeight(height);
+        } else {
+          Streamlit.setFrameHeight(0);
+        }
+      }
+    });
+
+    observer.observe(element);
+
+  }, []);
 
   if (action == "logout") {
     signOut()
@@ -63,7 +81,7 @@ function ClerkComponent({ args, theme }: ComponentProps): ReactElement {
     }
 
     if (isSignedIn && user) {
-      // Send the user object to Streamlit
+      // send user data to Streamlit
       const safeUser = {
         signedIn: isSignedIn,
         id: user.id,
@@ -77,14 +95,30 @@ function ClerkComponent({ args, theme }: ComponentProps): ReactElement {
   }, [isSignedIn, user]);
 
   if (!isSignedIn) {
-    return (
-      <span>
-        <SignIn 
-          routing="virtual"
-          forceRedirectUrl={COMPONENT_URL}
-        />
-      </span>
-    )
+    if (mode == "signin") {
+      return (
+        <div ref={containerRef}>
+          <SignIn 
+            routing="virtual"
+            forceRedirectUrl={COMPONENT_URL}
+            signUpForceRedirectUrl={COMPONENT_URL}
+            signUpUrl={`${COMPONENT_URL}?mode=signup`}
+          />
+        </div>
+      )
+    } else if (mode == "signup") {
+      return (
+        <div ref={containerRef}>
+          <SignUp
+            routing="virtual"
+            forceRedirectUrl={COMPONENT_URL}
+            signInForceRedirectUrl={COMPONENT_URL}
+            oauthFlow="popup"
+            signInUrl={`${COMPONENT_URL}?mode=signin`}
+          />
+        </div>
+      )
+    }
   }
   return (
     <span>

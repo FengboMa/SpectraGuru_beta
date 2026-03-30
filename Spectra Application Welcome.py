fbf2ab37f1997bfc33ebc9bb759f70a00625a4aa
Anthony Arminio <tonyarminio.0@gmail.com>
2026-03-30 13:19:47 -0400

    implements column width control for function usage table

--- Spectra Application Welcome.py
+++ Spectra Application Welcome.py
@@ -321,6 +321,7 @@ st.markdown(
 if 'function_count_data' not in st.session_state or not log.CULL_FUNCTION_TABLE_REFRESH:
     st.session_state.function_count_data = log.get_count_data()
 
+
 #st.dataframe(data=st.session_state.function_count_data, 
 #                column_config={
 #                    "Feature":st.column_config.TextColumn(width=200),

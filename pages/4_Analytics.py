400de20ffbd1c22ea8fcb715193441f1d27fc2b0
Emmanuel Pierre <esp48321@uga.edu>
2026-04-14 20:40:22 -0400

    Further improvements to help description for SVM's C attribute. Added more details

--- pages/4_Analytics.py
+++ pages/4_Analytics.py
@@ -190,10 +190,15 @@ if 'df' in st.session_state:
         st.sidebar.select_slider(label="t-SNE Perplexity", options=list(range(1,max_perplexity)),value=2, key="tSNE_perplexity")
         st.sidebar.select_slider(label="t-SNE Maximum number of iterations", options=list(range(200,1001)), value=500, key="tSNE_n_iter")
     elif st.session_state.stats_plot_select == "Support Vector Machine":
-
+        svm_kernel = st.selectbox(label="Select the Kernel for your Support Vector Machine", options = ["rbf", "polynomial", ""])
         st.sidebar.number_input(label='Regularization Term (C)',min_value= 1, max_value= 100, placeholder='Insert a number',
                                     key = 'svm_C', step = 1, value = 5,
-                                    help = "Penalty for misclassified points. Controls margin width vs misclassification tradeoff.")
+                                    help = ("Penalty for misclassified points. Controls margin width vs misclassification tradeoff.\n"
+                                            "Tradeoff between margin width and misclassification.\n" 
+                                            "Small C → wide margin, tolerates errors. Large C → narrow margin, fits training data tightly (risk of overfitting).\n"
+                                            "Applies to all kernel options"
+                                            )
+                                )
         
 
 # Stats section layout
